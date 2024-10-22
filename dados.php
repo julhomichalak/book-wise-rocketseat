f1661714a9298475cabf163c50756336600b43e2
Julho Michalak <julhomichalak@gmail.com>
2024-10-21 23:42:17 -0300

    estruturando mvc

---
+++ dados.php
@@ -0,0 +1,34 @@
+<?php
+
+$livros = [
+    [
+        'id' => 1,
+        'titulo' => '1984',
+        'autor' => 'George Orwell',
+        'descricao' => 'Um romance distópico sobre um futuro totalitário onde o governo controla todos os aspectos da vida.'
+    ],
+    [
+        'id' => 2,
+        'titulo' => 'Dom Quixote',
+        'autor' => 'Miguel de Cervantes',
+        'descricao' => 'A história de um fidalgo que, iludido pelos romances de cavalaria, decide tornar-se cavaleiro andante.'
+    ],
+    [
+        'id' => 3,
+        'titulo' => 'Moby Dick',
+        'autor' => 'Herman Melville',
+        'descricao' => 'A saga de uma caçada à baleia gigante, Moby Dick, liderada pelo obcecado Capitão Ahab.'
+    ],
+    [
+        'id' => 4,
+        'titulo' => 'Orgulho e Preconceito',
+        'autor' => 'Jane Austen',
+        'descricao' => 'Um romance sobre os problemas de casamento, moralidade e educação entre as classes sociais da Inglaterra do século XIX.'
+    ],
+    [
+        'id' => 5,
+        'titulo' => 'O Grande Gatsby',
+        'autor' => 'F. Scott Fitzgerald',
+        'descricao' => 'A trágica história do misterioso milionário Jay Gatsby e sua obsessão por Daisy Buchanan.'
+    ]
+];
--- index.php
+++ index.php
@@ -1,59 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Book Wise</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
+require 'dados.php';
+require 'functions.php';
 
-<body class="bg-stone-950 text-stone-200">
-    <header class=" bg-stone-900 ">
-        <nav class="mx-auto max-w-screen-lg flex justify-between  py-4">
-            <div class="font-bold text-xl tracking-wide">Book Wise</div>
-            <ul class="flex space-x-4 font-bold">
-                <li><a href="/" class="text-lime-500">Explorar</a></li>
-                <li><a href="/meus-livros.php" class="hover:underline">Meus livros</a></li>
-            </ul>
-            <ul>
-                <li><a href="/login.php">Login</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main class="mx-auto max-w-screen-lg  space-y-6 mt-6">
-        <div>
-
-            <form class="w-full flex space-x-2">
-                <input
-                    type="text"
-                    class=" border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
-                    placeholder="Pesquisar..."
-                    name="pesquisar" />
-
-                <button class="">🔎</button>
-            </form>
-        </div>
-
-
-        <section class="space-x-4">
-
-            <div class="w-1/3 p-2 rounded border-stone-800 border-2 bg-stone-900">
-                <div class="flex">
-                    <div class="w-1/3">Imagem</div>
-
-                    <div class="w-2/3">
-                        <div class="font-semibold">Titulo</div>
-                        <div class="text-xs italic">Autor</div>
-                        <div class="text-xs italic">⭐⭐⭐(3 Avaliação)</div>
-                    </div>
-                </div>
-                <div class="text-sm">Descrição</div>
-
-            </div>
-
-        </section>
-    </main>
-</body>
-
-</html>
+require 'routes.php';
